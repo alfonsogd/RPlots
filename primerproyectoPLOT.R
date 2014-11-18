d3a5806fa1500f07c4823211b44e050a22771355
alfonsogd <alfonso.gonzalezdamian@gmail.com>
2014-11-18 17:01:13 -0600

    Modificaciones
    Dos primeros plots ok, falta el tercero (combinar tres variables) y el
    cuarto, presentar cuatro plots, con dos nuevos.

--- primerproyectoPLOT.R
+++ primerproyectoPLOT.R
@@ -4,17 +4,19 @@ bd <- read.csv("~/Rall/household_power_consumption.txt", sep=";", na.strings="?"
 #convertir fechas
 library(plyr)
 library(lubridate)
-bd <- mutate(bd, DateTime = as.Date(dmy(Date)))
+bd <- mutate(bd, Date = as.Date(dmy(Date)))
 # alternativa creando otra variable
-bd <- mutate(bd, DateTime = ymd_hms(paste(bd$Date,bd$Time)))
 #subset solo febrero 2007 
 #We will only be using data from the dates 2007-02-01 and 2007-02-02 (2 dias)
 subbd <- bd[(bd$Date >= as.Date("2007/02/01") & bd$Date <= as.Date("2007/02/02")),]
+#convertir Time en class(date) strptime()
+subbd <- mutate(subbd, DateTime = ymd_hms(paste(subbd$Date,subbd$Time)))
 #Primer PLOT
 hist(subbd$Global_active_power, col="red",
      main="Global Active Power", 
      xlab= "Global Active Power (Kilowatts)")
 #Segundo PLOT
-#convertir Time en class(date) strptime()
-#subbd2 <- mutate(subbd, Time = hms(Time))
-plot (subbd$DateTime, subbd$Global_active_power)
+plot (subbd$DateTime, subbd$Global_active_power, type = "l", ylab="Global Active Power (Kilowatts)")
+#Tercer PLOT Tres colores para submetering
+plot (subbd$DateTime, subbd$Sub_metering_3, type = "l", ylab="Sub-metering" )
+# cuarto plot
